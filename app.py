0e5a318a6fd6645f276fb3a4a1d848855663f1db
khalilash <162676015+khalilash@users.noreply.github.com>
2026-05-11 17:46:49 +0700

    Change server port from 5000 to 8080

--- app.py
+++ app.py
@@ -99,5 +99,4 @@ def add_book():
 
 # A method that runs the application server.
 if __name__ == "__main__":
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(debug=False, threaded=True, port=5000)
+    app.run(host='0.0.0.0', debug=False, threaded=True, port=8080)
